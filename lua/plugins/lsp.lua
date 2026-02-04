d68eee4036ab219733ab3fdc2b757ddbe01c904c
Geeeean <violoni.gianluca@gmail.com>
2026-02-04 18:15:17 +0100

    fsharp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,22 +3,28 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             vim.lsp.config("ts_ls", {})
-            vim.lsp.enable({"ts_ls"})
+            vim.lsp.enable({ "ts_ls" })
 
             vim.lsp.config("pyright", {})
-            vim.lsp.enable({"pyright"})
+            vim.lsp.enable({ "pyright" })
 
             vim.lsp.config("tailwindcss", {})
-            vim.lsp.enable({"tailwindcss"})
+            vim.lsp.enable({ "tailwindcss" })
 
             vim.lsp.config("clangd", {})
-            vim.lsp.enable({"clangd"})
+            vim.lsp.enable({ "clangd" })
 
             vim.lsp.config("cssls", {})
-            vim.lsp.enable({"cssls"})
+            vim.lsp.enable({ "cssls" })
 
             vim.lsp.config("lua_ls", {})
-            vim.lsp.enable({"lua_ls"})
+            vim.lsp.enable({ "lua_ls" })
+
+            vim.lsp.config("fsautocomplete", {})
+            vim.lsp.enable({ "fsautocomplete" })
+
+            vim.lsp.config("fantomas", {})
+            vim.lsp.enable({ "fantomas" })
         end
     }
 }
